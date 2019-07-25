5744ce35b5aedc338fe6640cb48f411344e73a1b
johnericward <johnericward@gmail.com>
2019-07-24 23:06:10 -0400

    added to html, css, js files intitial set up and moment.js experiment

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+
+//====THIS WORKS TOO================
+// var today = new Date();
+// var currentTime = today.getTime()
+// console.log(currentTime);
+//==================================
+
+var currentTime = moment().format("D/M/YY hh:mm:ss A");
+console.log(currentTime);
